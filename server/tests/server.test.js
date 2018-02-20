e7033717ec36aa413cec2f41d562479e523d49c3
wannyin <ys764@cornell.edu>
2018-02-20 14:57:59 -0500

    add get test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text:'first'
+}, {
+    text:'second'
+}];
+
 beforeEach((done)=>{
-    Todo.remove({}).then(()=>done());
+    Todo.remove({}).then(()=>{
+        return Todo.insertMany(todos);
+    }).then(()=>done());
 });
 
 describe('POST/todos', ()=>{
@@ -24,11 +32,25 @@ describe('POST/todos', ()=>{
                 return done(err);
             }
 
-            Todo.find().then((todos)=>{
+            Todo.find({text}).then((todos)=>{
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
             }).catch((e)=> done(e));
         });
+    });
+});
+
+describe('GET/todos', ()=>{
+    it('should get all todos', (done)=>{
+
+        request(app)
+        .get('/todos')
+        .send({})
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
     });
 });
