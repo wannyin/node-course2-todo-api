0d9cd95dd380e360f2629e96fd86cd59faad149b
wannyin <ys764@cornell.edu>
2018-02-21 05:08:04 -0500

    add test get get todo id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text:'first'
 }, {
+    _id: new ObjectID(),
     text:'second'
 }];
 
@@ -50,6 +53,19 @@ describe('GET/todos', ()=>{
         .expect(200)
         .expect((res)=>{
             expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
+    });
+});
+
+describe('GET/todos/:id', ()=>{
+    it('should return todo doc', (done)=>{
+
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todo.text).toBe(todos[0].text);
         })
         .end(done);
     });
