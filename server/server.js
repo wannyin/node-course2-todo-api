fe74bb34dbdcce050561480a018fe35f9bd9cdb2
wannyin <ys764@cornell.edu>
2018-02-21 04:57:12 -0500

    add get by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -28,6 +29,25 @@ app.get('/todos',(req, res)=>{
         res.status(400).send(e);
     });
 });
+
+//GET /todos/123
+app.get('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        return res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    })
+})
 
 app.listen(3000, ()=>{
     console.log('started on port 3000');
