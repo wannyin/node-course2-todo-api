263be5140cd23198b6f874d2a4a0c65b54610572
wannyin <ys764@cornell.edu>
2018-02-22 03:40:44 -0500

    add auth middleware

--- server/models/user.js
+++ server/models/user.js
@@ -50,6 +50,28 @@ UserSchema.methods.generateAuthToken = function(){
     });
 };
 
+UserSchema.statics.findMyToken = function(token){
+    var User = this;
+    var decoded;
+
+    try{
+        decoded = jwt.verify(token, '123');
+    }
+    catch(e){
+        // return new Promise((resolve, reject)=>{
+        //     reject();
+        // });
+
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+}
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
