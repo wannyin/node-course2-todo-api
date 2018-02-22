d0794039aa07d447c1756ac303b97541ffe9d8d1
wannyin <ys764@cornell.edu>
2018-02-22 03:58:08 -0500

    hash before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,21 +1,35 @@
-const {SHA256} = require('crypto-js');
+// const {SHA256} = require('crypto-js');
 
-var message = 'Hello world';
-var hash = SHA256(message).toString();
+// var message = 'Hello world';
+// var hash = SHA256(message).toString();
 
-console.log(message);
-console.log(hash);
+// console.log(message);
+// console.log(hash);
 
 
 
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-// jwt.sign
-// jwt.verify
+// // jwt.sign
+// // jwt.verify
 
-var data = {id:10};
-var token = jwt.sign(data, '123');
-// console.log(token);
+// var data = {id:10};
+// var token = jwt.sign(data, '123');
+// // console.log(token);
 
-var decode = jwt.verify(token, '123');
-console.log(decode);
+// var decode = jwt.verify(token, '123');
+// console.log(decode);
+
+const bcrypt = require('bcryptjs');
+
+var password = '123abc';
+
+bcrypt.genSalt(10, (err, salt)=>{
+    bcrypt.hash(password, salt, (err, hash)=>{
+        console.log(hash);
+    })
+})
+
+bcrypt.compare(password, "$2a$10$XOYr3m/DQIlkzOZ9y5MJfObjFQx2jMk5DT/1H2vHsACmfbvIql64i", (err,res)=>{
+    console.log(res);
+})
