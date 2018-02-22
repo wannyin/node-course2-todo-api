b5dd3a7458f0f6e83855cfb94e746e6e229ea691
wannyin <ys764@cornell.edu>
2018-02-22 05:16:10 -0500

    add login

--- server/server.js
+++ server/server.js
@@ -101,6 +101,7 @@ app.patch('/todos/:id', (req, res)=>{
     });
 });
 
+
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
@@ -118,6 +119,17 @@ app.get('/users/me', authenticate, (req, res)=>{
     res.send(req.user);
 })
 
+app.post('/users/login', (req, res)=>{
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findMyCredentials(body.email, body.password).then((user)=>{
+        return user.generateAuthToken().then((token)=>{
+            res.header('x-auth', token).send(user);
+        })
+    }).catch((e)=>{
+        res.status(400).send();
+    });
+})
 
 app.listen(port, ()=>{
     console.log(`started on port ${port}`);
